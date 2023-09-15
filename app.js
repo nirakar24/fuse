45956969856c0b3de0efad28d465de2cf3e8357d
Nirakar Jena <jenashubham60@gmail.com>
2023-09-15 12:16:21 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const sec = document.querySelector(".sec .numb");
 const mili = document.querySelector(".mili .numb");
 var timer = setInterval(()=>{
   var currentDate = new Date().getTime();
-  var launchDate = new Date('sept 15, 2023 12:20:00').getTime();
+  var launchDate = new Date('sept 15, 2023 13:20:00').getTime();
   var duration = launchDate - currentDate;
 
   var minutes = Math.floor((duration % (1000 * 60 * 60)) / (1000 * 60));
